267831cc0d07f0fbe64dbe232fc338fc6be24132
yysn1 <124029692+yysn1@users.noreply.github.com>
2023-09-26 18:33:17 +0800

    Add files via upload

---
+++ TestBanking.java
@@ -0,0 +1,23 @@
+package banking;
+import banking.*;
+public class TestBanking {
+	public static void main(String[]args){
+		Customer customer;
+		Account account;
+		//Create an account that can has a 500.00 balance.
+		System.out.println("Creating the customer Jane Smith.");
+		customer=new Customer("Jane","Smith");
+		System.out.println("Creating her account with a 500.00 balance.");
+		customer.setAccount(new Account(500.00));
+		account=customer.getAccount();
+		//Perform some account transactions
+		System.out.println("Withdraw150.00:"+account.withdraw(150.00));
+		System.out.println("Deposit22.50:"+account.deposit(22.50));
+		System.out.println("Withdraw47.62:"+account.withdraw(47.62));
+		System.out.println("Withdraw400.00:"+account.withdraw(400.00));
+		//Print out the final account balance
+		System.out.println("Customer["+customer.getLastName()
+					+","+customer.getFirstName()
+					+"]hasabalanceof"+account.getBalance());		
+	}
+}
